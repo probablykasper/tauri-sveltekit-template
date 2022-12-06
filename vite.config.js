0df317f8afaff22b7e533d8e3363465924289993
Kasper <kasperkh.kh@gmail.com>
2022-12-06 23:34:09 +0100

    Remove vite-plugin-tauri to fix CI
    tauri-action and vite-plugin-tauri don't magically work together

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import sveltePreprocess from 'svelte-preprocess'
-import { tauri } from 'vite-plugin-tauri'
 
 export default defineConfig({
   root: './src',
   base: './', // use relative paths
   publicDir: '../public',
   clearScreen: false,
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
   build: {
     outDir: '../build',
     emptyOutDir: true,
@@ -16,7 +19,6 @@ export default defineConfig({
     target: ['chrome64', 'edge79', 'firefox62', 'safari11.1'],
   },
   plugins: [
-    tauri(),
     svelte({
       preprocess: sveltePreprocess(),
     }),
