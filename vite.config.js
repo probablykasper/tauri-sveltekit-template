6bbb981e17e2db97697a2bcd7f8fdfcb05aee881
Kasper <kasperkh.kh@gmail.com>
2022-12-06 23:23:30 +0100

    Use vite-plugin-tauri

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import sveltePreprocess from 'svelte-preprocess'
+import { tauri } from 'vite-plugin-tauri'
 
 export default defineConfig({
   root: './src',
   base: './', // use relative paths
   publicDir: '../public',
   clearScreen: false,
-  server: {
-    port: 3000,
-    strictPort: true,
-  },
   build: {
     outDir: '../build',
     emptyOutDir: true,
@@ -19,6 +16,7 @@ export default defineConfig({
     target: ['chrome64', 'edge79', 'firefox62', 'safari11.1'],
   },
   plugins: [
+    tauri(),
     svelte({
       preprocess: sveltePreprocess(),
     }),
