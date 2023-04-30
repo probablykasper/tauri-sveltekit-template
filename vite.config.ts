b80b2c9a6d4633d9fa8abe5c408bba828a951486
Kasper <kasperkh.kh@gmail.com>
2023-04-30 08:23:02 +0200

    Use vite defaults

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,12 @@ import { defineConfig } from 'vite'
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  root: './src',
-  base: './', // use relative paths
-  publicDir: '../public',
   clearScreen: false,
   server: {
     port: 3000,
     strictPort: true,
   },
   build: {
-    outDir: '../build',
-    emptyOutDir: true,
-    minify: false,
     sourcemap: true,
     target: ['chrome64', 'edge79', 'firefox62', 'safari11.1'],
   },
